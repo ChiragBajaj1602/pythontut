eece36ac9f4551fd5e5008cef03be2f3c6914a71
ChiragBajaj1602 <158046822+ChiragBajaj1602@users.noreply.github.com>
2024-02-01 14:35:20 +0530

    Update stringformatting.py
    added the fstring and format method

--- stringformatting.py
+++ stringformatting.py
@@ -1,4 +1,3 @@
-# used the string formatting
 name="John"
 print("my name is %s"%name)
 # to format a string with more than one varriables using tuples
@@ -7,3 +6,8 @@ age=21
 print("My name is %s and my age is %d"%(name,age))
 mylists=[1,2,3]
 print("A list: %s"%mylists)
+# string formatiing can also be achieved by the concept called fstring
+print(f"the string formating is done by {name}")
+# using the format method
+txt="for only {weight:.2f} the price is 45$"
+print(txt.format(weight=90))
