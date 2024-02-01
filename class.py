2647fa2932ffc9d2643b80c203c8ad531dc5dbc5
ChiragBajaj1602 <158046822+ChiragBajaj1602@users.noreply.github.com>
2024-02-01 17:12:59 +0530

    Update class.py
    added concepts of inheritance

--- class.py
+++ class.py
@@ -33,3 +33,28 @@ car2.value=10000
 # test code
 print(car1.description())
 print(car2.description())
+# practiced inheritance
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+   
+x = Student("Mike", "Olsen")
+x.printname()
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+  def welcome(self):
+    print("welcome {self.firstname} {self.lastname} to the class of {}")
+
+x = Student("Mike", "Olsen", 2019)
+x.welcome()
